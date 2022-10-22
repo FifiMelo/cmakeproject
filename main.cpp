3f22159c22f71a8584c549d37f6597394d72e36d
FifiMelo <felipe336336@gmail.com>
2022-10-22 15:03:46 +0200

    small changes

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 int main(int args, char** argv)
 {
     std::cout << "Ready" << std::endl;
-    SDL_INIT_EVERYTHING;
-    if(true) SDL_Init(SDL_INIT_EVERYTHING);
+    if(false) SDL_Init(SDL_INIT_EVERYTHING);
 	return 0;
 }
